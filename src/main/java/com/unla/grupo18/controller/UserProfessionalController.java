72d6822fed28e713f6500f69ff3e5bf4aa7b2b13
KaykiPrex <jair.garcia.motano@gmail.com>
2025-05-30 21:54:02 -0300

    feat: refactor user professional controller

--- src/main/java/com/unla/grupo18/controller/UserProfessionalController.java
+++ src/main/java/com/unla/grupo18/controller/UserProfessionalController.java
@@ -2,10 +2,12 @@ package com.unla.grupo18.controller;
 
 import com.unla.grupo18.model.Appointment;
 import com.unla.grupo18.services.AppointmentService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -21,10 +23,18 @@ public class UserProfessionalController {
     }
 
     @GetMapping("/home")
-    public String getDashboard(Model model) {
-        List<Appointment> appointments = appointmentService.getAppointmentsByProfessional(2);
-        model.addAttribute("appointments", appointments);
+    public String getHome(HttpSession session, Model model) {
+        Integer userId = (Integer) session.getAttribute("userId");
+
+        model.addAttribute("userId", userId);
         return "professional/home";
+    }
+
+    @GetMapping("/appointments/{userId}")
+    public String getAppointments(Model model, @PathVariable int userId) {
+        List<Appointment> appointments = appointmentService.getAppointmentsByProfessional(userId);
+        model.addAttribute("appointments", appointments);
+        return "professional/home/appointments";
     }
 
 }
