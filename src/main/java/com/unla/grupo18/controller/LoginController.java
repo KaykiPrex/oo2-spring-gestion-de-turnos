b6da3e5d0f6d3e8676290d7d8ae1ab0473ae2724
KaykiPrex <jair.garcia.motano@gmail.com>
2025-05-30 21:54:31 -0300

    feat: valided user session

--- src/main/java/com/unla/grupo18/controller/LoginController.java
+++ src/main/java/com/unla/grupo18/controller/LoginController.java
@@ -1,11 +1,23 @@
 package com.unla.grupo18.controller;
 
+import com.unla.grupo18.model.User;
+import com.unla.grupo18.repositories.IUserRepository;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Optional;
+
 @Controller
 public class LoginController {
+    private final IUserRepository userRepository;
+
+    public LoginController(IUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @GetMapping("/login")
     public String showLoginPage() {
@@ -13,8 +25,9 @@ public class LoginController {
     }
 
     @GetMapping("/login-success")
-    public String redirectByRole(Authentication authentication) {
+    public String redirectByRole(HttpSession session, Authentication authentication) {
         var authorities = authentication.getAuthorities();
+        validateUserSession(session);
 
         if (authorities.stream().anyMatch(a -> a.getAuthority().equals("admin"))) {
             return "redirect:users/admin/home";
@@ -25,5 +38,18 @@ public class LoginController {
         }
 
         return "redirect:/home";
+    }
+
+    private void validateUserSession(HttpSession session) {
+        Integer userId = (Integer) session.getAttribute("userId");
+
+        if (userId == null) {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication != null && authentication.getPrincipal() instanceof UserDetails userDetails) {
+                Optional<User> user = userRepository.findByUsername(userDetails.getUsername());
+                userId = user.map(User::getId).orElse(null);
+                session.setAttribute("userId", userId);
+            }
+        }
     }
 }
