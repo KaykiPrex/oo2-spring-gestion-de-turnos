8d1c3d73d7445169c92e11bbd6eda65da169201b
KaykiPrex <jair.garcia.motano@gmail.com>
2025-06-08 14:48:19 -0300

    feat: added MailSender Config

---
+++ src/main/java/com/unla/grupo18/infrastructure/notification/MailSender.java
@@ -0,0 +1,36 @@
+package com.unla.grupo18.infrastructure.notification;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
+
+@Service("MailSenderCustom")
+public class MailSender {
+    private final JavaMailSender mailSender;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final String MAIL_FROM = "test@gmail.com";
+    private final String ALIAS = "Notification Service";
+
+    public MailSender(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void send(String to, String subject, String message) throws MessagingException, UnsupportedEncodingException {
+        MimeMessage simpleMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(simpleMessage, true);
+
+        helper.setFrom(MAIL_FROM, ALIAS);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(message);
+        mailSender.send(simpleMessage);
+        logger.info("|JavaMailSender| Mail enviado exitosamente");
+    }
+
+}
---
+++ src/main/java/com/unla/grupo18/infrastructure/notification/MailSenderTitle.java
@@ -0,0 +1,6 @@
+package com.unla.grupo18.infrastructure.notification;
+
+public enum MailSenderTitle {
+    CANCEL_MAIL_CLIENT,
+    CANCEL_MAIL_PROFESSIONAL;
+}
