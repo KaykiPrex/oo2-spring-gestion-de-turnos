97ad3d95e5d742bb11c47eee2dab71414eb58b56
KaykiPrex <jair.garcia.motano@gmail.com>
2025-06-09 20:02:15 -0300

    feat: config swagger

--- src/main/java/com/unla/grupo18/controller/rest/UserController.java
+++ src/main/java/com/unla/grupo18/controller/rest/UserController.java
@@ -1,8 +1,13 @@
-package com.unla.grupo18.controller;
+package com.unla.grupo18.controller.rest;
 
 import com.unla.grupo18.services.UserService;
 import com.unla.grupo18.services.request.CreateUserRequest;
 import com.unla.grupo18.services.response.CreateUserResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +19,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 
 @RestController
-@RequestMapping("users")
+@RequestMapping("/api/users")
+@Tag(name = "Usuarios", description = "Operaciones relacionadas con la gestión de usuarios")
 public class UserController {
 
     final UserService service;
@@ -23,6 +29,8 @@ public class UserController {
         this.service = service;
     }
 
+    @Operation(summary = "crear nuevo usuario", description = "Registra un nuevo usuario en el sistema")
+    @ApiResponse(responseCode = "201",description = "description response", content = @Content(schema = @Schema(implementation = CreateUserResponse.class)))
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<CreateUserResponse> create(
--- src/main/java/com/unla/grupo18/infrastructure/SecurityConfig.java
+++ src/main/java/com/unla/grupo18/infrastructure/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -36,7 +37,7 @@ public class SecurityConfig {
                         .requestMatchers("/register").permitAll()
                         .requestMatchers("/css/**").permitAll()
                         .requestMatchers("/js/**").permitAll()
-                        .requestMatchers(HttpMethod.PUT, "/appointments/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/**").permitAll()
                         .requestMatchers(HttpMethod.PUT, "/calendar/**").permitAll()
                         .anyRequest().authenticated()
                 )
@@ -81,6 +82,12 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs*/**");
     }
 
 
