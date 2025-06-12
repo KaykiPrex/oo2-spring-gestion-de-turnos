0e47e1503fb273e6c3cf38069025a0fcc2483b57
nico <irreranicolas8@gmail.com>
2025-06-11 21:31:10 -0300

    feat: fix client turno controller.

--- src/main/java/com/unla/grupo18/controller/ClientTurnoController.java
+++ src/main/java/com/unla/grupo18/controller/ClientTurnoController.java
@@ -1,49 +1,38 @@
 package com.unla.grupo18.controller;
-import com.unla.grupo18.repositories.IAppointmentRepository;
-import com.unla.grupo18.model.Client;
-import com.unla.grupo18.model.Appointment;
-import com.unla.grupo18.model.ProfessionalService;
 
-import com.unla.grupo18.services.ProfessionalServiceService;
+import com.unla.grupo18.model.*;
+import com.unla.grupo18.services.AppointmentService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import java.util.List;
 
-
-import java.util.Optional;
-@Tag(name= "Controlador turno", description = "Controlador para sacar turno en cliente")
+@Tag(name = "Controlador turno", description = "Controlador para sacar turno en cliente")
 @RestController
 @RequestMapping("/calendar")
 public class ClientTurnoController {
 
     @Autowired
-    private IAppointmentRepository appointmentRepository;
+    private AppointmentService appointmentService;
+
     @Operation(summary = "Pedir turno", description = "Funcion para que el cliente pida un turno en especifico")
+    @ApiResponse(responseCode = "200", description = "Respuesta exitosa",
+            content = @Content(mediaType = "application/json"))
+    @ApiResponse(responseCode = "404", description = "Usuario no encontrado")
     @PutMapping("/{id}/pedir-turno")
     public ResponseEntity<?> assignClientToAppointment(@PathVariable Integer id) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Client client = (Client) authentication.getPrincipal();
 
-        System.out.println("cliente: " + client);
-        Optional<Appointment> optionalAppointment = appointmentRepository.findById(id);
-        if (optionalAppointment.isPresent()) {
-            com.unla.grupo18.model.Appointment appointment = optionalAppointment.get();
-
-            appointment.setClient(client);
-            appointmentRepository.save(appointment);
+        if (appointmentService.saveClient(id, client))
             return ResponseEntity.ok().body("Turno sacado correctamente");
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se pudo sacar turno");
-        }
+        else return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se pudo sacar turno");
     }
 
 
--- src/main/java/com/unla/grupo18/services/AppointmentService.java
+++ src/main/java/com/unla/grupo18/services/AppointmentService.java
@@ -1,12 +1,11 @@
 package com.unla.grupo18.services;
-
 import com.unla.grupo18.model.Appointment;
 import com.unla.grupo18.model.Client;
 import com.unla.grupo18.repositories.IAppointmentRepository;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AppointmentService {
@@ -19,9 +18,11 @@ public class AppointmentService {
     public List<Appointment> getAppointmentsByProfessional(int professionalId) {
         return appointmentRepository.findByProfessionalId(professionalId);
     }
+
     public List<Appointment> getAppointmentsByToday(int professionalId) {
         return appointmentRepository.findBydate(LocalDate.now());
     }
+
     public List<Appointment> getAppointmentsByClient(int clientId) {
         return appointmentRepository.findByClientId(clientId);
     }
@@ -30,7 +31,7 @@ public class AppointmentService {
         return appointmentRepository.findDistinctClientsByProfessional(professionalId);
     }
 
-    public Appointment  getAppointmentsById(int id) {
+    public Appointment getAppointmentsById(int id) {
         return appointmentRepository.findById(id).orElseThrow();
     }
 
@@ -38,4 +39,16 @@ public class AppointmentService {
         appointmentRepository.save(appointment);
     }
 
+    public boolean saveClient(int idAppointment, Client client) {
+
+        Optional<Appointment> optionalAppointment = appointmentRepository.findById(idAppointment);
+        if (optionalAppointment.isPresent()) {
+            com.unla.grupo18.model.Appointment appointment = optionalAppointment.get();
+            appointment.setClient(client);
+            appointmentRepository.save(appointment);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
