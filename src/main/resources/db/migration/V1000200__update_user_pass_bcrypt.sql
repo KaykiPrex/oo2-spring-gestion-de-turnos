0dad2cb2123f36cbafc04c64fdfd69bea2fa061b
KaykiPrex <jair.garcia.motano@gmail.com>
2025-06-19 00:24:15 -0300

    fix: encrypt password with Bcrypt

---
+++ src/main/resources/db/migration/V1000200__update_user_pass_bcrypt.sql
@@ -0,0 +1,9 @@
+-- USERS BCRYPT
+UPDATE user
+SET password = '$2a$10$7bE228WRxTyvc//TWyRS0uI.CEBUIjJSILAAGPiqnzNVCJWPdZM52'
+
+WHERE id = 1;
+
+UPDATE user
+SET password = '$2a$10$YBonlbEk701ug.bQVhTgLeJefrfnWMmMBXdfMJs8v6oyp9iy09PUW'
+WHERE id = 2;
