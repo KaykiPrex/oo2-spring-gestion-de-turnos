ab56ee001a7024e0e7db3d554e5f84cb8e23d29d
nico <irreranicolas8@gmail.com>
2025-06-20 15:23:39 -0300

    fix: registerController

--- src/main/java/com/unla/grupo18/controller/RegisterController.java
+++ src/main/java/com/unla/grupo18/controller/RegisterController.java
@@ -5,6 +5,8 @@ import com.unla.grupo18.model.Client;
 import com.unla.grupo18.model.Role;
 import com.unla.grupo18.repositories.IClientRepository;
 import com.unla.grupo18.repositories.IRoleRepository;
+import com.unla.grupo18.services.clientService;
+import com.unla.grupo18.services.roleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
@@ -18,15 +20,13 @@ import java.util.Map;
 @Controller
 public class RegisterController{
     @Autowired
-    private final IClientRepository clientRepository;
+    private final clientService clientService;
     @Autowired
-    private final IRoleRepository roleRepository;
 
 
-    public RegisterController(IClientRepository clientRepository, IRoleRepository roleRepository) {
-        this.clientRepository = clientRepository;
-        this.roleRepository = roleRepository;
 
+    public RegisterController(clientService clientService) {
+        this.clientService = clientService;
     }
 
     @GetMapping("/register")
@@ -35,26 +35,15 @@ public class RegisterController{
     }
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody Map<String, String> payload) {
-        System.out.println("Estas en la funcion de registrar usuario");
+
         String username = payload.get("username");
         String password = payload.get("password");
         String name = payload.get("name");
         String lastName = payload.get("lastName");
         String dni = payload.get("dni");
 
-        if (clientRepository.findByUsername(username).isPresent()) {
-            return ResponseEntity.ok("El nombre de usuario ya está en uso");
-        }
-
-        Client client = new Client(username, password, name, lastName, dni);
-
-        System.out.println(client);
-        Role role = roleRepository.findByName("client")
-            .orElseThrow(() -> new RuntimeException("Rol no encontrado"));
-        client.setRole(role);
-        clientRepository.save(client);
+        clientService.createClient(username, password, name, lastName, dni);
 
         return ResponseEntity.ok("Usuario registrado correctamente");
     }
-
 }
